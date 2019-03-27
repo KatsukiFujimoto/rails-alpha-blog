3ffc2bf7a6ad4b828085763e093ae0d83e79001c
vagrant <vagrant@localhost.localdomain>
2019-03-27 14:54:02 +0000

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
